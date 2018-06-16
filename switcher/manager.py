af0e4d74a5eb8998653eaf5906247d50314a60b1
ArthurSamuelyan <artur.samuelyan@phystech.edu>
2018-06-16 17:20:11 +0300

    Rollback added

--- switcher/manager.py
+++ switcher/manager.py
@@ -1,8 +1,8 @@
 import ctypes
 import socket
-import os
-#import shutil
+import sys, os
 import codecs
+import signal, psutil
 
 def DestroyDirectory( dir_path ):
     for file_name in os.listdir( dir_path ):
@@ -27,86 +27,90 @@ class Manager:
     """Manager implements spfs-manager interface"""
 
 
-    def __socket_path( self ):
-        return "%s/%s" % ( self.work_dir, self.socket_file )
+    #def __socket_path( self ):
+    #    return "%s/%s" % ( self.work_dir, self.socket_file )
 
     def __init__( self, new_socket_file ):
 
+        # TODO: rewrite using "os.join"
         self.manager  = "spfs-manager"
-        self.work_dir = "./work-dir"
-        self.spfs_mnt = self.work_dir + "/spfs-mnt"
+        self.swd = os.path.join( os.getcwd(), "work-dir" ) # SWD = Switcher Work Directory
+        self.work_dir = os.path.join( self.swd, "manager-work-dir" )
+        self.spfs_mnt = os.path.join( self.swd, "spfs-mnt" )
         self.log_dir  = "log-dir"
         self.socket_file = "control.sock"
+        self.socket_path = os.path.join( self.work_dir, self.socket_file )
+        self.history = [] # NEW FEATURE
 
-        self.namespaces = ["pid"]        # Do we need it?
+        # Refreshing wokr-dir:
+	if( os.access( self.swd, os.F_OK ) ):
+	    DestroyDirectory( self.swd )
 
-        #============================== unmounting spfs ==
-	#pid = os.fork()
-        #if pid > 0:
-        #    pid, status = os.waitpid( pid, 0 )
-        #else:
-        #    os.execvp( "umount", [ "-l", self.spfs_mnt ] )
-        #=================================================
+        if( os.path.exists( self.swd ) ):
+            print "Failed to clean switcher work directory!"
+            sys.exit()
+        
+        os.mkdir( self.swd, 0755 )
 
-	if( os.access( self.work_dir, os.F_OK ) ):
-	    DestroyDirectory( self.work_dir )
-
-        #============================== creating directories =====
-        try: # do we need here try/except ?
-            os.mkdir( self.work_dir, 0755 )
-        except OSError:
-            print "Work directory already exists!"
+        os.mkdir( self.work_dir, 0755 )
         self.work_dir = os.path.abspath( self.work_dir )
 
-	try:
-            os.mkdir( self.spfs_mnt, 0755 )
-        except OSError:
-            print self.spfs_mnt + " alreadt exists!"
+        os.mkdir( self.spfs_mnt, 0755 )
         self.spfs_mnt = os.path.abspath( self.spfs_mnt )
-        #=========================================================
+        # ===================
 
-        print "Socket: " + self.__socket_path()
+        print "SPFS manager socket: " + self.socket_path
 
-        self.socket_path = new_socket_file # ???
-        if os.path.exists( self.__socket_path() ):
-            os.remove( self.__socket_path() )
+        #if os.path.exists( self.socket_path ):
+        #    os.remove( self.socket_path )
 
         pid = os.fork()
         if pid > 0:
             pid, status = os.waitpid(pid, 0)
         else:
+            # shall we use subprocess here?
             os.execvp( self.manager, [
                 "spfs-manager",
                 "-vvvv",
                 "-d",
-                "--socket-path", self.socket_file,
+                "--socket-path", self.socket_file, # relative
                 "--work-dir",    self.work_dir,
-                "--log-dir",     self.log_dir,
+                "--log-dir",     self.log_dir, # relative
                 "--exit-with-spfs"
             ] )
 
-
-    def __send_request( self, req_type, **kwargs ):
-        request = req_type
-        for key in kwargs:
-            request += "%s=%s;" % (key, kwargs[key])
-        request += '\0'
-
+    def __send_premade_request( self, req_type, request ):
         self.sock = socket.socket(socket.AF_UNIX, socket.SOCK_SEQPACKET)
-        self.sock.connect( self.__socket_path() )
+        self.sock.connect( self.socket_path )
         try:
-            self.sock.send(request, socket.MSG_EOR)
-            status = self.sock.recv(4)
-            print( "Request %s status %d." % ( req_type, int( codecs.encode( status, 'hex'), 16 ) ) )
+            self.sock.send( request, socket.MSG_EOR )
+            status_str = self.sock.recv(4)
+            status = int( codecs.encode( status_str, 'hex'), 16 )
+            print( "SPFS manager: %s status %d." % ( req_type, status ) )
             self.sock.close()
-            return 0
+            if status == 0:
+                return 0
+            else:
+                return 1
 	except socket.timeout:
             self.sock.close()
             return -1
 
+    def __send_request( self, req_type, **kwargs ):
+        history_string = [] # NEW FEATURE
+        history_string.append( req_type ) #
+        history_string.append( kwargs ) #
+        self.history.append( history_string ) #
+
+        request = req_type
+        for key in kwargs:
+            request += "%s=%s;" % (key, kwargs[key])
+        request += '\0'
+        return self.__send_premade_request( req_type, request )
+
     def mount( self, **kwargs ):
         req = "mount;"
-        return self.__send_request( req, **kwargs )
+	return self.__send_request( req, **kwargs )
 
     def set_mode( self, **kwargs ):
         req = "mode;"
@@ -115,3 +119,17 @@ class Manager:
     def switch( self, **kwargs ):
         req = "switch;"
         return self.__send_request( req, **kwargs )
+
+    def rollback( self ):
+        print "Rollback started."
+        self.history.reverse()
+        if self.history[ 0 ][ 0 ] == "mode;":
+            print "\"mode\" rollback was not provided :("
+        for h_line in self.history[ 1: ]: # the last command was failure, we don't reverse it
+            if h_line[ 0 ] == "switch;":
+                request = "switch;source=%s;target=%s;freeze_cgroup=%s;\0" % ( h_line[ 1 ][ "target" ], h_line[ 1 ][ "source" ], h_line[ 1 ][ "freeze_cgroup" ] )
+                if self.__send_premade_request( h_line[ 0 ], request ) != 0:
+                    print "Rollback failed!"
+                    return -1
+        print "Rollback completed."
+        return 0;
--- switcher/switch.py
+++ switcher/switch.py
@@ -5,14 +5,7 @@ import manager
 import sys
 import os
 import argparse
-import subprocess
-
-def CreateParser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument( '-p', '--pid', nargs='+', required=True )
-    parser.add_argument( '-s', '--src', required=True )
-    parser.add_argument( '-d', '--dst', required=True )
-    return parser
+import subprocess # Shall we use it for manager.py too?
 
 def FreezerPath( freezer_name ):
     return "/sys/fs/cgroup/freezer/" + freezer_name
@@ -20,29 +13,8 @@ def FreezerPath( freezer_name ):
 def TasksPath( freezer_name ):
     return FreezerPath( freezer_name ) + "/tasks"
 
-def PutProcessesIntoFreezer( processes, freezer_name ):
-    try:
-        os.mkdir( FreezerPath( freezer_name ), 0755)
-    except OSError:
-        print freezer_name + " already exists!"
-    else:
-        print freezer_name + " created!"
-    for _pid in processes:
-
-        if ( CheckRoot( _pid ) == 0 ):
-            freezer_tasks = open( TasksPath( freezer_name ), 'a' )
-            freezer_tasks.write( "%s\n" % ( _pid ) )
-            try:
-                freezer_tasks.close()
-            except IOError:
-                print( _pid + " [-]" )
-            else:
-                print( _pid + " [+]" )
-        else:
-            print( _pid + " [-]" )
-
 def ParseTasks( freezer_name ):
-    tasks_file = open( TasksPath( freezer_name ), 'r' )
+    tasks_file = open( TasksPath( freezer_name ), "r" )
     tasks = list()
     _pid = tasks_file.readline()
     while _pid:
@@ -50,6 +22,33 @@ def ParseTasks( freezer_name ):
         _pid = tasks_file.readline()
     tasks_file.close()
     return tasks
+
+def PutProcessesIntoFreezer( pids, freezer_name ):
+    try:
+        os.mkdir( FreezerPath( freezer_name ), 0755)
+    except OSError:
+        print "Freezer %s already exists!" % ( freezer_name )
+    else:
+        print "Freezer %s created!" % ( freezer_name )
+
+    if len( pids ) == 0:
+        return -1
+    else:
+        print "Putting processes into %s freezer:" % ( freezer_name )
+        for _pid in pids:
+            freezer_tasks = open( TasksPath( freezer_name ), "a" )
+            freezer_tasks.write( "%s\n" % ( _pid ) )
+            try:
+                freezer_tasks.close()
+            except IOError:
+                print( _pid + " [-]" )
+            else:
+                print( _pid + " [+]" )
+        if len( ParseTasks( freezer_name ) ) == 0:
+            return -1
+        else:
+            return 0
+        
 
 def RsyncCall( abs_src, abs_dst ):
     proc = subprocess.Popen( "rsync -avzh %s/. %s" % ( abs_src, abs_dst ), shell=True, stdout=subprocess.PIPE )
@@ -60,101 +59,132 @@ def FindMountPoint( abs_path ):
         abs_path = os.path.dirname( abs_path )
     return abs_path
 
-def CheckRoot( pid ): 
-    # root process always has uid=0
-    # uid is the 15th (from 0) word in /proc/<pid>/stat file
-    stat_file = "/proc/%s/status" % ( pid )
-    if os.access( stat_file, os.R_OK ):
-        f = open( stat_file, "r" )
+def GetStatusString( pid, str_num ):
+    status_file = "/proc/%s/status" % ( pid )
+    if os.access( status_file, os.R_OK ):
+        f = open( status_file, "r" )
         data = f.read()
         f.close()
         lines = data.split('\n')
-        words = lines[ 8 ].split('\t')
-        #print( words )
-
-        if( words[ 1 ] == "0" ):
-            # the process exists and is root
-            return 0
-        else:
-            # process is non root
-            return -1
-
+        words = lines[ str_num ].split('\t')
+        return words
     else:
-        # process does not exist
+        raise OSError
+
+
+def CheckRoot( pid ): 
+    try:
+        words = GetStatusString( pid, 8 )
+        if( words[ 1 ] == "0" ): # the process exists and is root
+            return 0
+        else: # process is non root
+            return -1
+    except OSError: # process does not exist
         return -2
 
+def PickRoots( pids ):
+    root_pids = []
+    rejected_pids = []
+    for pid in pids:
+        if CheckRoot( pid ) != 10: # == 0
+            root_pids.append( pid )
+        else:
+            rejected_pids.append( pid )
+    # Print rejected PIDs:
+    if len( rejected_pids ) > 0:
+        print( "Theese PIDs are rejected, and will not be switched" )
+        for r_pid in rejected_pids:
+            print( r_pid + " [-]" )
+
+    return root_pids
+
+
+
+def CreateParser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument( '-p', '--pid', nargs='+', required=True )
+    parser.add_argument( '-s', '--src', required=True )
+    parser.add_argument( '-d', '--dst', required=True )
+    return parser
 
 def main():
-    # ===Preparation:==================================================================================
-    # ===Parse command line arguments:
+    # === Preparation: ====
+    # Parse command line arguments:
     parser = CreateParser()
     arguments = parser.parse_args( sys.argv[1:] )
     if( ( not os.path.isdir( arguments.src ) ) or ( not os.path.isdir( arguments.dst ) ) ):
         print( "Error: --src and --dst values must be directories!" )
         return
- 
-    print( arguments ) # debug
 
-    # ===We'll need two cgroups: spfs_switcher_active and spfs_switcher_retired
-    # ===We'll give spfs_switcher_active controls to spfs-manager and it may freeze or unfreeze this cgroup
-    # ===Because the only way to delete the process from a cgroup is to put it into another cgroup,
-    # ===we'll create spfs_switcher_retired. This cgroup shall never be freezed. We'll put there processes from
-    # ===spfs_switcher_active after unfreezing them via spfs-manager
+    # We'll need two cgroups: spfs_switcher_active and spfs_switcher_retired
+    # We'll give spfs_switcher_active controls to spfs-manager and it may freeze or unfreeze this cgroup
+    # Because the only way to delete the process from a cgroup is to put it into another cgroup,
+    # we'll create spfs_switcher_retired. This cgroup shall never be freezed. We'll put there processes from
+    # spfs_switcher_active after unfreezing them via spfs-manager
     freezer_name_a = "spfs_switcher_active"
     freezer_name_r = "spfs_switcher_retired" 
     abs_src = os.path.abspath( arguments.src )
     abs_dst = os.path.abspath( arguments.dst )
     
-    # ===Checking mount points. spfs-manager needs src and dst directories to be on different mount points.
+    # Checking mount points. spfs-manager needs src and dst directories to be on different mount points.
     mnt_src = FindMountPoint( abs_src )
     mnt_dst = FindMountPoint( abs_dst )
     if( mnt_src == mnt_dst ):
         print( "Error: --src and --dst directories must be on different mount points!" )
         return
 
-    # ===Launching and preparing spfs-manager:
+    # Launching and preparing spfs-manager:
     mngr = manager.Manager( "./control.sock" )    # Shall we allow user to specify control socket location?
 
-    # mnt_spfs = mngr.work_dir + "/spfs-mnt"   # Shall we allow user to specify mnt directory?
-#    try:
-#        os.mkdir( mnt_spfs, 0755 )
-#    except OSError:
-#        print( mnt_spfs + " already exists." )
-
-#    mnt_spfs = os.path.abspath( mnt_spfs )
-
-    # ===Mounting spfs into mnt_spfs on src directory:
-    mngr.mount( id=0, mountpoint=mngr.spfs_mnt, mode="proxy", proxy_dir=abs_src )
-    # ===Creating spfs_switcher_active freezer cgroup and filling with given processes: 
-    PutProcessesIntoFreezer( arguments.pid, freezer_name_a )
-    # ===Preliminary data syncing between src and dst (in order to speed up main data syncing): 
+    # Mounting spfs into mnt_spfs on src directory:
+    if mngr.mount( id=0, mountpoint=mngr.spfs_mnt, mode="proxy", proxy_dir=abs_src ) != 0:
+        print "Failed to mount SPFS!"
+        return
+    # Creating spfs_switcher_active freezer cgroup and filling with given processes: 
+    if PutProcessesIntoFreezer( PickRoots( arguments.pid ), freezer_name_a ) != 0:
+        print "Nothing to switch!"
+        return
+    # Preliminary data syncing between src and dst (in order to speed up main data syncing): 
+    print "Preliminary syncing started."
     RsyncCall( abs_src, abs_dst )
-    # ===Preparation complete.=========================================================================
+    # === Preparation complete. ===
 
-    # ===Switch:=======================================================================================
-    # ===1: Switching processes src -> spfs-mnt:
-    mngr.switch( source=abs_src, target=mngr.spfs_mnt, freeze_cgroup=FreezerPath( freezer_name_a ) )
+    # === Switch: ===
+    # 1: Switching processes src -> spfs-mnt:
+    if mngr.switch( source=abs_src, target=mngr.spfs_mnt, freeze_cgroup=FreezerPath( freezer_name_a ) ) != 0:
+	mngr.rollback()
+        print "Switch aborted!"
+        return
 
-    # ===2: Synchronize data:
-    # ===   Freezing processes in cgroup:
-    mngr.set_mode( id=0, mode="stub" )
-    # ===   Main data syncing:
+    # 2: Synchronize data:
+    #    Freezing processes in cgroup:
+    if mngr.set_mode( id=0, mode="stub" ) != 0:
+	mngr.rollback()
+        print "Switch aborted!"
+        return
+
+    #    Main data syncing:
     RsyncCall( abs_src, abs_dst )
-    # ===   Unfreezing processes back:
-    mngr.set_mode( id=0, mode="proxy", proxy_dir=abs_dst )
+    #    Unfreezing processes back:
+    if mngr.set_mode( id=0, mode="proxy", proxy_dir=abs_src ) != 0: # was dst. Debug?
+	mngr.rollback()
+        print "Switch aborted!"
+        return
 
-    # ===3: Switching processes spfs-mnt -> dst:
-    mngr.switch( source=mngr.spfs_mnt, target=abs_dst, freeze_cgroup=FreezerPath( freezer_name_a ) )
-    # ===Switch complete.==============================================================================
+
+    # 3: Switching processes spfs-mnt -> dst:
+    if mngr.switch( source=mngr.spfs_mnt, target=abs_dst, freeze_cgroup=FreezerPath( freezer_name_a ) ) != 0:
+	mngr.rollback()
+        print "Switch aborted!"
+        return
+
+    # === Switch complete. ===
 
     # Cleaning spfs_switcher_active freezer:
     PutProcessesIntoFreezer( ParseTasks( freezer_name_a ), freezer_name_r )
 
-#    pid = os.fork()
-#    if pid > 0:
-#        pid, status = os.waitpid( pid, 0 ) 
-#    else:
-#        os.execvp( "umount", [ "umount", "-l", mngr.spfs_mnt] )
+    print "Switch complete!"
+
 
 if __name__ == '__main__':
     main()
